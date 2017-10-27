b6e39244c2db39057e9c337592463a63832991f2
slimcdk <cs@slimc.dk>
2017-10-27 12:23:29 +0200

    added collision and roll-a-ball test player

---
+++ Assets/Scripts/Debugging/roll-a-ball-test/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private GameObject[] getCount;
+    public GameObject prefab;
+    private int count;
+    private int treats;
+
+
+    void Start()
+    {
+    
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        
+        treats = Random.Range(5, 20);
+
+        for (var i = 0; i < treats; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-9.0f, 9.0f), 0.5f, Random.Range(-9.0f, 9.0f));
+            Instantiate(prefab, position, Quaternion.identity);
+       
+        }
+        
+        // getCount = GameObject.FindGameObjectsWithTag("Pick Up");
+        // treats = getCount.Length;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-moveHorizontal, 0.0f, -moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= treats)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+}
